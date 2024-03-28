5a42c891183300c6367318020a2ec7ad03f3a5dc
gitaunthama <gitaunthama@gmail.com>
2024-03-28 11:38:59 +0300

    complete work

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your code here!
+
+var elementToRemove= document.getElementById('main');
+
+var parentElement= elementToRemove.parentNode;
+
+parentElement.removeChild(elementToRemove);
+
+
+var newHeader = document.createElement('h1');
+newHeader.textContent = 'This is a new header';
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+
+var newHeader = document.createElement('h1');
+newHeader.textContent = 'Grace is the champion'; 
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+
+
